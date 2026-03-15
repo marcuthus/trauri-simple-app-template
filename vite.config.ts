b37d3623f7a6b6e64737633667ef87d7345a6778
Marcuth <thiago.playgames15@gmail.com>
2026-03-15 14:15:47 -0300

    add path resolve config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import path from "node:path"
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST
 
 // https://vite.dev/config/
@@ -28,6 +28,11 @@ export default defineConfig(async () => ({
         watch: {
             // 3. tell Vite to ignore watching `src-tauri`
             ignored: ["**/src-tauri/**"],
+        },
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
         },
     },
 }))
